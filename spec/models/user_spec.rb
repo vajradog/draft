cc87e91694dcecc23ae40856f4bf23e688b4836b
vajradog <vajradog@gmail.com>
2014-05-09 23:13:57 -0400

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,23 +14,28 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
-
-
+  it { should respond_to(:admin) }
   it { should be_valid }
+  it { should_not be_admin}
 
-  describe "when name is not present" do 
-    before { @user.name= " "}
-    it {should_not be_valid}
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
   end
 
-  describe "when email is not present" do 
-    before { @user.email= " "}
+  describe "when name is not present" do
+    before { @user.name = " " }
     it { should_not be_valid }
   end
 
-  describe "when name is more than 50 characters" do 
-    before { @user.name = "a" * 51}
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
@@ -44,22 +49,23 @@ describe User do
     end
   end
 
-  describe "when email format is invalid" do 
-    it "should be invalid" do 
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+bar.com]
+describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
-        expect(@user).not_to be_valid
+        @user.should_not be_valid
+      end
     end
   end
-end
 
-  describe "when email is valid" do 
-    it "should be valid" do 
-      addresses = %w[user@foo.COM A-US-ER@f.b.org frst.1st@foo.jp a+b@baz.cn]
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
-        expect(@user).to be_valid
+        @user.should be_valid
       end
     end
   end
@@ -75,11 +81,8 @@ end
 
   end
 
-  describe "when password is not present" do
-    before do
-      @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
-    end
+describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
 
@@ -88,29 +91,36 @@ end
     it { should_not be_valid }
   end
 
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
   end
+
+  describe "when password is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should_not be_valid }
+  end
+
 
   describe "return value of authenticate method" do
     before { @user.save }
-    let(:found_user) { User.find_by(email: @user.email) }
+    let(:found_user) { User.find_by_email(@user.email) }
 
     describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
+      it { should == found_user.authenticate(@user.password) }
     end
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-      it { should_not eq user_for_invalid_password }
-      specify { expect(user_for_invalid_password).to be_false }
+      
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
     end
+  end
 
-    describe "remember token" do
+  describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
-  end
+  
 end
